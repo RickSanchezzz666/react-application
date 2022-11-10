b30a59993e70b5ec83983b97ea61cbc74ecb5fe7
RickSanchezzz666 <lapiyktube@gmail.com>
2022-11-10 15:48:38 +0200

    ue

--- src/components/component1/index.js
+++ src/components/component1/index.js
@@ -10,75 +10,48 @@ function Component1() {
         image: image1,
         title: 'Headline Text',
         text1: 'Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.',
-        text2: 'Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.'
+        text2: 'Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.',
+        button: 'Read More »'
     },
     {
         image: image1,
         title: 'Headline Text',
         text1: 'Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.',
-        text2: 'Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.'
+        text2: 'Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.',
+        button: 'Read More »'
     },
     {
         image: image1,
         title: 'Headline Text',
         text1: 'Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.',
-        text2: 'Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.'
+        text2: 'Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.',
+        button: 'Read More »'
     },
     {
         image: image1,
         title: 'Headline Text',
         text1: 'Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.',
-        text2: 'Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.'
+        text2: 'Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.',
+        button: 'Read More »'
     },
 ];
 
     return(
         <div className="page1">
             <div className="container1">
-            {textik
-            .map(card => <Paragraph
-            image={card.image}
-            title={card.title}
-            text1={card.text1}
-            text2={card.text2}
-            />)}
-                <div className="item-1">
-                    <img src={image1} className="img-square"></img>
-                        <div className="paragraph"><span className="title">Headline Text</span>
-                        <p className="text">Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.<br></br><br></br>
-                        Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.
-                        </p>
-                        <a href="">Read More »</a>
-                    </div>
-                </div>
-                <div className="item-2">
-                <img src={image1} className="img-square"></img>
-                    <div className="paragraph"><span className="title">Headline Text</span>
-                    <p className="text">Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.<br></br><br></br>
-                    Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.
-                    </p>
-                    <a href="">Read More »</a>
-                </div>
-                </div>
-                <div className="item-3">
-                <img src={image1} className="img-square"></img>
-                    <div className="paragraph"><span className="title">Headline Text</span>
-                    <p className="text">Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.<br></br><br></br>
-                    Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.
-                    </p>
-                    <a href="">Read More »</a>
-                </div>
-                </div>
-                <div className="item-4"><img src={image1} className="img-square"></img>
-                    <div className="paragraph"><span className="title">Headline Text</span>
-                    <p className="text">Integer imperdiet vestibulum leo ut tincidunt. In sagittis diam ut leo convallis vel rutrum mauris ullamcorper. Vestibulum adipiscing erat sit amet justo luctus molestie.<br></br><br></br>
-                    Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.
-                    </p>
-                    <a href="">Read More »</a>
-                </div>
+                {textik
+                .map(card => <Paragraph
+                image={card.image}
+                title={card.title}
+                text1={card.text1}
+                text2={card.text2}
+                button={card.button}
+                />)}
+                <hr></hr><hr></hr>
             </div>
-            <hr></hr><hr></hr>
-        </div>
+            <div className="container2">
+                <div>its ok</div>
+            </div>
     </div>
     );
 }
--- src/components/component1/paragraph/index.js
+++ src/components/component1/paragraph/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import './style.css';
 
 function Paragraph(props) {
     return(
         <div className="text-page">
+            <div className="item">
             <img src={props.image}></img>
+            <div className="paragraph">
             <span className="title">{props.title}</span>
-            <p className="text1">{props.text1}</p>
-            <br></br><br></br>
-            <p className="text2">{props.text2}</p>
+            <p className="text">{props.text1}</p>
+            <br></br>
+            <p className="text">{props.text2}</p>
+            <a href="">{props.button}</a>
+            </div>
+            </div>
         </div>
     );
 }
