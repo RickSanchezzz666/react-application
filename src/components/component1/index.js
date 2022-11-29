4488d287784a103e8c465e2a86f62fed0e184783
RickSanchezzz666 <lapiyktube@gmail.com>
2022-11-29 12:04:01 +0200

    nav bar

--- src/components/component1/index.js
+++ src/components/component1/index.js
@@ -3,14 +3,49 @@ import './style.css';
 
 function Component1() {
 
-    const pagesRef = document.getElementById('pages-dropdown')
+    const pagesRef = document.getElementById('pages-dropdown');
+    const elementsRef = document.getElementById('elements-dropdown');
+    const portfolioRef = document.getElementById('portfolio-dropdown');
+    const galleryRef = document.getElementById('gallery-dropdown');
 
     const onMouseEnterPages = function() {
         pagesRef.classList.add('drop-active')
+        pagesRef.classList.remove('drop-unactive')
     }
 
     const onMouseLeavePages = function() {
-        pagesRef.classList.replace('drop-active')
+        pagesRef.classList.remove('drop-active')
+        pagesRef.classList.add('drop-unactive')
+    }
+
+    const onMouseEnterElements = function() {
+        elementsRef.classList.add('drop-active')
+        elementsRef.classList.remove('drop-unactive')
+    }
+
+    const onMouseLeaveElements = function() {
+        elementsRef.classList.remove('drop-active')
+        elementsRef.classList.add('drop-unactive')
+    }
+
+    const onMouseEnterPortfolio = function() {
+        portfolioRef.classList.add('drop-active')
+        portfolioRef.classList.remove('drop-unactive')
+    }
+
+    const onMouseLeavePortfolio = function() {
+        portfolioRef.classList.remove('drop-active')
+        portfolioRef.classList.add('drop-unactive')
+    }
+
+    const onMouseEnterGallery= function() {
+        galleryRef.classList.add('drop-active')
+        galleryRef.classList.remove('drop-unactive')
+    }
+
+    const onMouseLeaveGallery = function() {
+        galleryRef.classList.remove('drop-active')
+        galleryRef.classList.add('drop-unactive')
     }
 
     return(
@@ -19,7 +54,7 @@ function Component1() {
                 
             </div>
             <div className="page-1-container">
-                        <ul id="pages-dropdown">
+                        <ul id="pages-dropdown" className="drop-unactive">
                             <li>Full Width Content</li>
                             <li>Content + Left Sidebar</li>
                             <li>Content + Right Sidebar</li>
@@ -31,7 +66,7 @@ function Component1() {
                             <li>Team Member</li>
                             <li>FAQ</li>
                         </ul>
-                        <ul className="elements-dropdown">
+                        <ul id="elements-dropdown" className=" drop-unactive">
                             <li>Buttons</li>
                             <li>Alert Messages</li>
                             <li>Font Icons</li>
@@ -44,7 +79,7 @@ function Component1() {
                             <li>Toggles</li>
                             <li>Pricing Tables</li>
                         </ul>
-                        <ul className="portfolio-dropdown">
+                        <ul id="portfolio-dropdown" className=" drop-unactive">
                             <li>Full Width Portfolio</li>
                             <li>2 Column Grid</li>
                             <li>2 Col. Grid + Left Sidebar</li>
@@ -56,7 +91,7 @@ function Component1() {
                             <li>3 Col. Grid + Both Sidebars</li>
                             <li>4 Column Grid</li>
                         </ul>
-                        <ul className="gallery-dropdown">
+                        <ul id="gallery-dropdown" className=" drop-unactive">
                             <li>Full Width Gallery</li>
                             <li>2 Column Grid</li>
                             <li>2 Col. Grid + Left Sidebar</li>
@@ -74,9 +109,15 @@ function Component1() {
                     <div onMouseEnter={onMouseEnterPages} 
                          onMouseLeave={onMouseLeavePages}
                     className="pages">↓ Pages</div>
-                    <div className="elements">↓ Elements</div>
-                    <div className="portfolio">↓ Portfolio Layouts</div>
-                    <div className="gallery">↓ Gallery layouts</div>
+                    <div onMouseEnter={onMouseEnterElements} 
+                        onMouseLeave={onMouseLeaveElements}
+                    className="elements">↓ Elements</div>
+                    <div onMouseEnter={onMouseEnterPortfolio} 
+                         onMouseLeave={onMouseLeavePortfolio}
+                    className="portfolio">↓ Portfolio Layouts</div>
+                    <div onMouseEnter={onMouseEnterGallery} 
+                         onMouseLeave={onMouseLeaveGallery}
+                     className="gallery">↓ Gallery layouts</div>
                     <div>Link text</div>
                     <div>A VERY LONG LINK TEXT</div>
                 </div>
