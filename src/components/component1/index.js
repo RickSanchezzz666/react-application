25db917fd7ac2d9f6f06708fee1154c6711b00b8
RickSanchezzz666 <lapiyktube@gmail.com>
2022-11-29 15:33:25 +0200

    final

--- src/components/component1/index.js
+++ src/components/component1/index.js
@@ -1,57 +1,61 @@
 import React from "react";
 import './style.css';
+import mailImg from '../imgs/mail.png';
+import callimg from '../imgs/call.png';
 
 function Component1() {
-
-    const pagesRef = document.getElementById('pages-dropdown');
-    const elementsRef = document.getElementById('elements-dropdown');
-    const portfolioRef = document.getElementById('portfolio-dropdown');
-    const galleryRef = document.getElementById('gallery-dropdown');
-
-    const onMouseEnterPages = function() {
-        pagesRef.classList.add('drop-active')
-        pagesRef.classList.remove('drop-unactive')
-    }
-
-    const onMouseLeavePages = function() {
-        pagesRef.classList.remove('drop-active')
-        pagesRef.classList.add('drop-unactive')
-    }
-
-    const onMouseEnterElements = function() {
-        elementsRef.classList.add('drop-active')
-        elementsRef.classList.remove('drop-unactive')
-    }
-
-    const onMouseLeaveElements = function() {
-        elementsRef.classList.remove('drop-active')
-        elementsRef.classList.add('drop-unactive')
-    }
-
-    const onMouseEnterPortfolio = function() {
-        portfolioRef.classList.add('drop-active')
-        portfolioRef.classList.remove('drop-unactive')
-    }
-
-    const onMouseLeavePortfolio = function() {
-        portfolioRef.classList.remove('drop-active')
-        portfolioRef.classList.add('drop-unactive')
-    }
-
-    const onMouseEnterGallery= function() {
-        galleryRef.classList.add('drop-active')
-        galleryRef.classList.remove('drop-unactive')
-    }
-
-    const onMouseLeaveGallery = function() {
-        galleryRef.classList.remove('drop-active')
-        galleryRef.classList.add('drop-unactive')
-    }
+    
+        const pagesRef = document.getElementById('pages-dropdown');
+        const elementsRef = document.getElementById('elements-dropdown');
+        const portfolioRef = document.getElementById('portfolio-dropdown');
+        const galleryRef = document.getElementById('gallery-dropdown');
+    
+        const onMouseEnterPages = function() {
+            pagesRef.classList.add('drop-active')
+            pagesRef.classList.remove('drop-unactive')
+        }
+    
+        const onMouseLeavePages = function() {
+            pagesRef.classList.remove('drop-active')
+            pagesRef.classList.add('drop-unactive')
+        }
+    
+        const onMouseEnterElements = function() {
+            elementsRef.classList.add('drop-active')
+            elementsRef.classList.remove('drop-unactive')
+        }
+    
+        const onMouseLeaveElements = function() {
+            elementsRef.classList.remove('drop-active')
+            elementsRef.classList.add('drop-unactive')
+        }
+    
+        const onMouseEnterPortfolio = function() {
+            portfolioRef.classList.add('drop-active')
+            portfolioRef.classList.remove('drop-unactive')
+        }
+    
+        const onMouseLeavePortfolio = function() {
+            portfolioRef.classList.remove('drop-active')
+            portfolioRef.classList.add('drop-unactive')
+        }
+    
+        const onMouseEnterGallery= function() {
+            galleryRef.classList.add('drop-active')
+            galleryRef.classList.remove('drop-unactive')
+        }
+    
+        const onMouseLeaveGallery = function() {
+            galleryRef.classList.remove('drop-active')
+            galleryRef.classList.add('drop-unactive')
+        }
 
     return(
         <div className="page-1">
             <div className="page-1-top">
-                
+                <div className="page-1-grid-1"><span className="header-title">RS-1200 Prototype 35</span>
+                <span className="header-sub-title">Free Responsive Template</span></div>
+                <div className="page-1-container-2"><img className="mailImage" src={mailImg}></img>info@domain.com  | <img className="callImage" src={callimg}></img> +xx xxx xxxxxxxxxx</div>
             </div>
             <div className="page-1-container">
                         <ul id="pages-dropdown" className="drop-unactive">
@@ -66,7 +70,7 @@ function Component1() {
                             <li>Team Member</li>
                             <li>FAQ</li>
                         </ul>
-                        <ul id="elements-dropdown" className=" drop-unactive">
+                        <ul id="elements-dropdown" className="drop-unactive">
                             <li>Buttons</li>
                             <li>Alert Messages</li>
                             <li>Font Icons</li>
@@ -79,7 +83,7 @@ function Component1() {
                             <li>Toggles</li>
                             <li>Pricing Tables</li>
                         </ul>
-                        <ul id="portfolio-dropdown" className=" drop-unactive">
+                        <ul id="portfolio-dropdown" className="drop-unactive">
                             <li>Full Width Portfolio</li>
                             <li>2 Column Grid</li>
                             <li>2 Col. Grid + Left Sidebar</li>
@@ -91,7 +95,7 @@ function Component1() {
                             <li>3 Col. Grid + Both Sidebars</li>
                             <li>4 Column Grid</li>
                         </ul>
-                        <ul id="gallery-dropdown" className=" drop-unactive">
+                        <ul id="gallery-dropdown" className="drop-unactive">
                             <li>Full Width Gallery</li>
                             <li>2 Column Grid</li>
                             <li>2 Col. Grid + Left Sidebar</li>
@@ -123,7 +127,7 @@ function Component1() {
                 </div>
             </div>
         </div>
-    );
+    ); 
 }
 
 export default Component1;
