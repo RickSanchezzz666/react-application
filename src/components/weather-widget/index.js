71a4f92abb017e09ff6889e4aa1e7ace8d97d957
RickSanchezzz666 <lapiyktube@gmail.com>
2022-12-05 19:45:18 +0200

    upd

--- src/components/weather-widget/index.js
+++ src/components/weather-widget/index.js
@@ -7,31 +7,46 @@ function WeatherWidget (props) {
  const [data, setData] = useState(weatherMockData);
 
  useEffect(() => {
-  console.log('step 1');
   axios
-   .get(`http://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=`)
+   .get(`http://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=03224bd9ced99c81469eae9d7c7e2675`)
    .then(response => {
     setData(response.data);
-    console.log('step 2');
    });
-  console.log('step 3');
 
  }, [props.city]);
 
  return (
   <div className="widget-weather">
-   City: {props.city}
+    <div className="ww-place">
+        <b>Country:</b> {data.sys.country}
+        <b>City:</b> {props.city}
+    </div>
    <div className="ww-coord">
-    <div>Latitude:</div><div>{data.coord.lat}</div>
-    <div>Longitude:</div><div>{data.coord.lon}</div>
+    <div><b>Latitude</b>:</div><div>{data.coord.lat}</div>
+    <div><b>Longitude</b>:</div><div>{data.coord.lon}</div>
    </div>
    <div className="ww-weather">
     {data.weather.map(el => (
      <div>
-      <img src={`http://openweathermap.org/img/w/${el.icon}.png`} alt='' />
-      Description: {el.description}
+        <img src={`http://openweathermap.org/img/w/${el.icon}.png`} alt='' />
+        <b>Description</b>: {el.description}
      </div>
     ))}
+     <div>
+        <b>Temperature</b>: {data.main.temp}<br></br>
+        <b>Pressure</b>: {data.main.pressure}<br></br>
+        <b>Humidity</b>: {data.main.humidity}
+     </div>
+     <div>
+        <b>Wind</b>:<br></br>
+        <b>Speed</b>: {data.wind.speed}<br></br>
+        <b>Degrees</b>: {data.wind.deg}<br></br>
+        <b>Gust</b>: {data.wind.gust}
+     </div>
+     <div>
+        <b>Visibility</b>: {data.visibility} km<br></br>
+        <b>Clouds</b>: {data.clouds.all}%<br></br>
+     </div>
    </div>
   </div>
  );
