f414cb1891c676b962a77c4062c76a6a4bc81c24
RickSanchezzz666 <lapiyktube@gmail.com>
2022-12-05 12:02:02 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import Component1 from './components/component1';
-import Component2 from './components/component2';
-import Component3 from './components/component3';
-import Component4 from './components/component4';
-import Mercury from './components/component3/mercury';
-import Venus from './components/component3/venus';
-import Jupiter from './components/component3/jupiter';
-import Saturn from './components/component3/saturn';
-import Uranus from './components/component3/uranus';
-import Neptune from './components/component3/neptune';
-import Contact from './components/contact'
-import Error from './components/error';
-import { useEffect } from 'react';
+import WeatherWidget from './components/weather-widget';
 
-function App() {
-    useEffect(() => {
-        window.history.scrollRestoration = 'manual'
-      }, []);
+function App () {
+  const [selectedCity, setSelectedCity] = useState();
 
-    return(
-            <div className='pages'>
-                <div className='page-1'><Component1 /></div>
-                <div className='page-2'><Component2 /></div>
-                <div className='page-3'>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path='/mercury' element={<Mercury />} />
-                        <Route path='/venus' element={<Venus />} />
-                        <Route path='/' element={<Component3 />} />
-                        <Route path='/jupiter' element={<Jupiter />} />
-                        <Route path='/saturn' element={<Saturn />} />
-                        <Route path='/uranus' element={<Uranus />} />
-                        <Route path='/neptune' element={<Neptune />} />
-                        <Route path='/contact' element={<Contact />} />
-                        <Route path='*' element={<Error />} />
-                    </Routes>
-                </BrowserRouter>
-                </div>
-                <div className='page-4'><Component4 /></div>
-            </div>
-        
-    );
+  const selectCity = (event) => {
+    const value = event.target.value;
+    setSelectedCity(value);
+  };
+
+  console.log(`selectedCity:${selectedCity}`);
+  return (
+    <div className="App">
+      <div className='input-wrapper'>
+        <input
+          onBlur={selectCity}
+          type="text" class="input-search" id="input-search" />
+        <label class="search" for="input-search">
+        </label>
+      </div>
+      <WeatherWidget city={selectedCity} />
+    </div>
+  );
 }
 
 export default App;
