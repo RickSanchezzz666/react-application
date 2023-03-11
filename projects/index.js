3d865b7ed61922b0b73c1b61fdc337d9d041ae08
RickSanchezzz666 <lapiyktube@gmail.com>
2023-03-11 17:01:22 +0200

    push

--- projects/index.js
+++ projects/index.js
@@ -7,21 +7,27 @@ const scenario = fs.readFileSync('./scenario.txt', (err, data) => {
     }
     console.log(data);
 })
-const resultio = scenario.toString();
-const results = resultio.match(/^[a-z]+:/gmi);
+const resultNames = scenario.toString();
+const results_1 = resultNames.match(/^[a-z]+:/gmi);
+
+const resultText = scenario.toString();
+const results_2 = resultText.match(/[a-z]+.+[?,;.:!]/gmi);
 
 const charachters = [];
-results.forEach(characterName => {
+const charachtersSpeeches = [];
+results_1.forEach(characterName => {
  const name = characterName.slice(0, -1);
- // [].find(e => ...) => null || elem
- // [].includes(<value>) => boolean
- if (!charachters.includes(name)) {
-  charachters.push(name);
- }
-});
+ results_2.forEach(texts => {
+  //const text = texts.slice(-(name.length + 1), 0);
+    if (!charachters.includes(name)) {
+      charachters.push(name);
+    }
+    charachtersSpeeches.push(texts);
+  })
+})
 
 console.log(charachters);
 
 for(let num = 0; num < charachters.length; num += 1) {
-    fs.writeFileSync(`${charachters[num]}.txt`, 'blablabla')
+      fs.writeFileSync(`${charachters[num]}.txt`, `${charachtersSpeeches[num]}`)
 }
