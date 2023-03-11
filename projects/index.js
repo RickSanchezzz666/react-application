c271a041019839918e23191b4d992c05335a135b
RickSanchezzz666 <lapiyktube@gmail.com>
2023-03-11 20:55:27 +0200

    asdas

--- projects/index.js
+++ projects/index.js
@@ -17,21 +17,30 @@ const charachters = [];
 const charachtersSpeeches = [];
 results_1.forEach(characterName => {
  const name = characterName.slice(0, -1);
- results_2.forEach(texts => {
-  //const text = texts.slice(-(name.length + 1), 0);
     if (!charachters.includes(name)) {
       charachters.push(name);
     }
-    for(let num = 0; charachters.length; num += 1) {
-      if(results_1 === charachters[num]) {
-        charachtersSpeeches`${charachters[num]}`.push(texts);
-      }
-    }
-  })
+})
+
+results_2.forEach(texts => {
+  const text = texts.slice(-5, 0);
+  
+  if(text.match(charachters[0])) {
+    charachtersSpeeches[0].push(texts)
+  }
+  if(text.match(charachters[1])) {
+    charachtersSpeeches[1].push(texts)
+  }
+  if(text.match(charachters[2])) {
+    charachtersSpeeches[2].push(texts)
+  }
+  if(text.match(charachters[3])) {
+    charachtersSpeeches[3].push(texts)
+  }
 })
 
 console.log(charachters);
 
 for(let num = 0; num < charachters.length; num += 1) {
-      fs.writeFileSync(`${charachters[num]}.txt`, `${charachtersSpeeches`${charachters[num]}`}`)
+      fs.writeFileSync(`${charachters[num]}.txt`, `${charachtersSpeeches[num]}`)
 }
